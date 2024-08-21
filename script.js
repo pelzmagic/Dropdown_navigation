10a5242b7d0517f285a64db2641373f1c2e0b179
pelzmagic <akinloyepelz@gmail.com>
2024-08-21 14:02:13 +0100

    update desktop version with complete functionality

--- script.js
+++ script.js
@@ -4,10 +4,24 @@ const featuresArrowDown = document.querySelector(".features_arrow_down");
 const companyArrowDown = document.querySelector(".company_arrow_down");
 const featuresSubMenu = document.querySelector(".sub_menu1");
 const companySubMenu = document.querySelector(".sub_menu2");
+const button = document.querySelector(".dropdown_toggle");
+const companyButton = document.querySelector(".company_dropdown_toggle");
 
-featuresArrowDown.addEventListener("click", (e) => {
-    featuresSubMenu.style.display = "block";
+button.addEventListener("click", (e) => {
+    if (featuresSubMenu.style.display === "block") {
+        featuresSubMenu.style.display = "none";
+        featuresArrowDown.style.transform = "rotate(0deg)";
+    } else {
+        featuresSubMenu.style.display = "block";
+        featuresArrowDown.style.transform = "rotate(180deg)";
+    }
 })
-companyArrowDown.addEventListener("click", (e) => {
-    companySubMenu.style.display = "block";
+companyButton.addEventListener("click", (e) => {
+    if (companySubMenu.style.display === "none") {
+        companySubMenu.style.display = "block";
+        companyArrowDown.style.transform = "rotate(180deg)";
+    } else {
+        companySubMenu.style.display = "none";
+        companyArrowDown.style.transform = "rotate(0deg)";
+    }
 })
